dd460beeca694b999e43581c175eb23027eaaf6a
lpianta <pianta.luca@gmail.com>
2021-07-01 19:45:59 +0200

    backend

--- rock_paper_scissor/gui.py
+++ rock_paper_scissor/gui.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 from tkinter import ttk
 
@@ -11,6 +12,36 @@ human_img = "imgs/human.png"
 computer_img = "imgs/robot.png"
 win_human_img = "imgs/human_biceps.png"
 win_computer_img = "imgs/robot_biceps.png"
+
+
+class Game(object):
+
+    def __init__(self):
+        self.choices = ['rock', 'paper', 'scissors']
+        self.victory = [
+            ['rock', 'scissors'],
+            ['paper', 'rock'],
+            ['scissors', 'paper']
+        ]
+        self.player_score = 0
+        self.computer_score = 0
+
+    def _get_rules(self, list_of_rules):
+        self.choices.extend(list(set(list_of_rules)))
+        self.victory.extend(list_of_rules)
+
+    def player_input(self, command):
+        self.player_choice = command
+        print(self.player_choice)
+        return self.player_choice
+
+    def computer_input(self):
+        self.computer_choice = random.choice(self.choices)
+        return self.computer_choice
+
+    def compare_choices(self, player_choice, computer_choice):
+        match = [player_choice, computer_choice]
+        # if len(set(match)) == 1:
 
 
 class RockPaperScissors(tk.Tk):
@@ -65,6 +96,7 @@ class StartPage(tk.Frame):
 class PlayerVsComputer(tk.Frame):
 
     def __init__(self, parent, controller):
+        game = Game()
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Player VS computer", font=LARGE_FONT)
         label.grid(columnspan=5)
@@ -74,11 +106,12 @@ class PlayerVsComputer(tk.Frame):
         self.scissors_img = tk.PhotoImage(file=scissors_img)
 
         rock_btn = tk.Button(self, text="Rock!",
-                             image=self.rock_img, compound="bottom")
+                             image=self.rock_img, compound="bottom", command=lambda *args: game.player_input("rock"))
+
         paper_btn = tk.Button(self, text="Paper!",
-                              image=self.paper_img, compound="bottom")
+                              image=self.paper_img, compound="bottom", command=lambda *args: game.player_input("paper"))
         scissors_btn = tk.Button(
-            self, text="Scissors!", image=self.scissors_img, compound="bottom")
+            self, text="Scissors!", image=self.scissors_img, compound="bottom", command=lambda *args: game.player_input("scissors"))
 
         rock_btn.grid(row=1, column=1)
         paper_btn.grid(row=1, column=2)
@@ -88,8 +121,9 @@ class PlayerVsComputer(tk.Frame):
                                   command=lambda: controller.show_frame(StartPage))
         startpage_btn.grid(columnspan=5)
 
-        human_score = tk.Label(self, text="Your Score: 0")
-        computer_score = tk.Label(self, text="Computer Score: 0")
+        human_score = tk.Label(self, text=f"Your Score: {game.player_score}")
+        computer_score = tk.Label(
+            self, text=f"Computer Score: {game.computer_score}")
         human_score.grid(columnspan=5)
         computer_score.grid(columnspan=5)
 
